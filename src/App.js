48517317bbe64f71c95ac3798aa6e3a7e141a36f
Abhii30 <akumar300599@gmail.com>
2023-01-09 20:22:51 +0530

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Book from "./Components/book";
+import CheckIn from "./Components/checkIn";
+import Manage from "./Components/manage";
+import Navbar from "./Components/navbar";
+import SignUp from "./Components/signUp";
+import SignIn from "./Components/signIn";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/book" element={<Book />} />
+          <Route path="/checkIn" element={<CheckIn />} />
+          <Route path="/manage" element={<Manage />} />
+          <Route path="/signIn" element={<SignIn />} />
+          <Route path="/signUp" element={<SignUp />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/navbar.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import navStyles from "./navbar.module.css";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import Logo from "../Resource/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+export default function Navbar() {
+  return (
+    <div>
+      <nav className={navStyles.navbar}>
+        <Link to="/">
+          <div className={navStyles.col_logo}>
+            <img src={Logo} alt="Virgin America" />
+          </div>
+        </Link>
+        <div className={navStyles.vl}></div>
+        <div className={navStyles.col_tabs}>
+          <Link to="/book" className={navStyles.tabs}>
+            Book
+          </Link>
+          <Link to="/checkIn" className={navStyles.tabs}>
+            Check In
+          </Link>
+          <Link to="/manage" className={navStyles.tabs}>
+            Manage
+          </Link>
+        </div>
+        <div className={navStyles.vl}></div>
+        <div className={navStyles.links}>
+          <a href="#">Deals</a>
+          <a href="#">Flying With Us</a>
+          <a href="#">Where We Fly</a>
+          <a href="#">Shop</a>
+          <a href="#">FAQ</a>
+          <a href="#">Fees</a>
+        </div>
+        <div className={navStyles.access}>
+          <Link to="/signIn" className={navStyles.tabs1}>
+            Sign In
+          </Link>
+          <Link to="/signUp" className={navStyles.tabs}>
+            Sign Up
+          </Link>
+          <div className={navStyles.vl1}></div>
+          <FontAwesomeIcon icon={faBars} className={navStyles.dropdown} />
+        </div>
+      </nav>
+    </div>
+  );
+}
